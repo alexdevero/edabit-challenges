b63386cedacf2ae8906b8a86e647811e7a534a1d
Alex Devero <deveroalex@gmail.com>
2019-10-26 07:46:39 +0200

    Fix hot-pics-of-danny-devito

--- strings/js-hot-pics-of-danny-devito.js
+++ strings/js-hot-pics-of-danny-devito.js
@@ -2,8 +2,8 @@ function preventDistractions(str) {
 	return (str.match(/anime/) || str.match(/meme/) || str.match(/vine/) || str.match(/roasts/) || str.match(/Danny Devito/)) ? 'NO!' : 'Safe watching!'
 }
 
-'vines that butter my eggroll') // 'NO!'
-'Hot pictures of Danny Devito') // 'NO!'
-'top 10 insert random yt celebrity roasts') // 'NO!'
-'best anime food ever!') // 'NO!'
-'How to Be a Productive Member of Society') // 'Safe watching!'
+preventDistractions('vines that butter my eggroll') // 'NO!'
+preventDistractions('Hot pictures of Danny Devito') // 'NO!'
+preventDistractions('top 10 insert random yt celebrity roasts') // 'NO!'
+preventDistractions('best anime food ever!') // 'NO!'
+preventDistractions('How to Be a Productive Member of Society') // 'Safe watching!'
