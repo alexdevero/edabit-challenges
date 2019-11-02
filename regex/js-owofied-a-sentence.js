8a3c5a1ae048b31f99fa3c4e93aaf1f2237322e7
Alex Devero <deveroalex@gmail.com>
2019-11-02 11:27:14 +0100

    Add owofied-a-sentence

---
+++ regex/js-owofied-a-sentence.js
@@ -0,0 +1,11 @@
+function owofied(sentence) {
+	return sentence.replace(/i/g, 'wi').replace(/e/g, 'we') + ' owo'
+}
+
+owofied("I'm gonna ride 'til I can't no more") // "I'm gonna rwidwe 'twil I can't no morwe owo"
+owofied("Do you ever feel like a plastic bag") // "Do you wevwer fwewel lwikwe a plastwic bag owo"
+owofied("Cause baby you're a firework") // "Causwe baby you'rwe a fwirwework owo"
+owofied("Never gonna give you up") // "Nwevwer gonna gwivwe you up owo"
+owofied("We've known each other for so long") // "Wwe'vwe known weach othwer for so long owo"
+owofied("Never gonna let you down") // "Nwevwer gonna lwet you down owo"
+owofied("Shine bright like a diamond") // "Shwinwe brwight lwikwe a dwiamond owo"
