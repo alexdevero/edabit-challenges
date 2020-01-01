3b3a346e8c5e92cb425ec617215ea8d4ce609d42
Alex Devero <deveroalex@gmail.com>
2020-01-01 09:09:42 +0100

    Add less-than-100

---
+++ math/js-less-than-100.js
@@ -0,0 +1,24 @@
+function lessThan100(a, b) {
+	return a + b < 100 ? true : false
+}
+
+lessThan100(5, 57) // true
+lessThan100(77, 30) // false
+lessThan100(0, 59) // true
+lessThan100(78, 35) // false
+lessThan100(63, 11) // true
+lessThan100(37, 99) // false
+lessThan100(52, 11) // true
+lessThan100(82, 95) // false
+lessThan100(17, 44) // true
+lessThan100(74, 53) // false
+lessThan100(3, 77) // true
+lessThan100(25, 80) // false
+lessThan100(59, 28) // true
+lessThan100(69, 87) // false
+lessThan100(10, 45) // true
+lessThan100(43, 58) // false
+lessThan100(50, 44) // true
+lessThan100(74, 89) // false
+lessThan100(3, 27) // true
+lessThan100(21, 79) // false
