dc6fc5861598b25f2af96bf58bd847c806408d41
Alex Devero <deveroalex@gmail.com>
2020-01-02 08:08:30 +0100

    Add count-the-capital-letters

---
+++ strings/js-count-the-capital-letters.js
@@ -0,0 +1,24 @@
+function capitalLetters(txt) {
+	return txt.split('').filter(x => /[A-Z]/g.test(x)).length
+}
+
+capitalLetters("hvbHKuFxMORMuBYRsgNF") //  12
+capitalLetters("nrrvrXlmfwjwlbcjwrzt") //  1
+capitalLetters("VdefGMkIeTUipnLhnIfe") //  8
+capitalLetters("ShwvYyeQdGsJYRBhoihI") //  9
+capitalLetters("ykEblUluPthiCMhxQFlz") //  7
+capitalLetters("NCdLUzNdihwUumkqfJqN") //  8
+capitalLetters("sdbMinvzxxulkzyzhfWe") //  2
+capitalLetters("NEcLjmRMJEXwnBkiPpCO") //  12
+capitalLetters("KVFifdDbnuWdkDdmDAkO") //  9
+capitalLetters("pkgUmnquvzIueBlLbtYu") //  5
+capitalLetters("xbbpykGhycjVEvCdaMew") //  5
+capitalLetters("YVgbDlgZDDaGkOymseSP") //  10
+capitalLetters("miuyhtTofqSuoCrgyhtc") //  3
+capitalLetters("YYoNuXdYJhHIPFZZndya") //  12
+capitalLetters("LRuGZepMZYIwQWXJpoTB") //  14
+capitalLetters("gmEosHVTLNEsYOFLCrRR") //  14
+capitalLetters("dPiadMjurjCjrgYbceuK") //  5
+capitalLetters("PQgSxLADOswVPWoDBgCD") //  14
+capitalLetters("odoaxlnmuyixnpthidus") //  0
+capitalLetters("XcqykqdmvtuMynFhnayf") //  3
