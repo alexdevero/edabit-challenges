bc913833dc442bfe41f0868fce410ba3862fc06d
Alex Devero <deveroalex@gmail.com>
2020-01-12 05:27:48 +0100

    Add free-coffee-cups

---
+++ math/js-free-coffee-cups.js
@@ -0,0 +1,10 @@
+function totalCups(n) {
+	return Math.floor((n / 6) + n)
+}
+
+totalCups(6) // 7
+totalCups(3) // 3
+totalCups(7) // 8
+totalCups(12) // 14
+totalCups(213) // 248
+totalCups(16) // 18
