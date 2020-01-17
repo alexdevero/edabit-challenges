b8fa93121262490dfef5331a34f1dd0dcf3f17ef
Alex Devero <deveroalex@gmail.com>
2020-01-17 08:52:35 +0100

    Add last-character-of-first-name

---
+++ strings/js-last-character-of-first-name.js
@@ -0,0 +1,11 @@
+function isLastCharacterN(word) {
+	return word.endsWith('n')
+}
+
+isLastCharacterN("Aiden") // true
+isLastCharacterN("Roxy") // false
+isLastCharacterN("Bert") // false
+isLastCharacterN("Dean") // true
+isLastCharacterN("Ian") // true
+isLastCharacterN("Brian") // true
+isLastCharacterN("Daniel") // falsed
