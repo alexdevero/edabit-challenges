aece5ebe5f83358459ff858872572e00679b6c4b
Alex Devero <deveroalex@gmail.com>
2020-01-30 06:13:21 +0100

    Add multiply-every-array-item-by-two

---
+++ math/js-multiply-every-array-item-by-two.js
@@ -0,0 +1,7 @@
+function getMultipliedArr(arr) {
+	return arr.map(num => num * 2)
+}
+
+getMultipliedArr([2, 5, 3]) // [4, 10, 6]
+getMultipliedArr([1, 86, -5]) // [2, 172, -10]
+getMultipliedArr([5, 382, 0]) // [10, 764, 0]
