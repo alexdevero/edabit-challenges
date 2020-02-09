b2f179ef5daa62a110090c039ca9f6d8d13d76bc
Alex Devero <deveroalex@gmail.com>
2020-02-09 07:56:44 +0100

    Add es6-destructuring-arrays-iii

---
+++ arrays/js-es6-destructuring-arrays-iii.js
@@ -0,0 +1,6 @@
+// DO NOT change arr
+// DO NOT USE var [lips] = arr[2]
+// "eyes", "nose", and "ears" should not be assigned to anything
+
+const arr = ["eyes", "nose", "lips", "ears"]
+var lips = arr[2]
