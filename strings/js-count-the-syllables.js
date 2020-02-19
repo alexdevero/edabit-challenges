647a4ad7aa718ca2c769ec9208e2e69e64119b63
Alex Devero <deveroalex@gmail.com>
2020-02-19 08:01:26 +0100

    Add count-the-syllables

---
+++ strings/js-count-the-syllables.js
@@ -0,0 +1,13 @@
+function countSyllables(str) {
+	return str.length / 2
+}
+
+countSyllables("Hehehehehehe") // 6
+countSyllables("bobobobobobobobo") // 8
+countSyllables("NANANANA") // 4
+countSyllables("lelelele") // 4
+countSyllables("momomomomomomomomo") // 9
+countSyllables("WiWiWiWiWiWiWiWiWiWi") // 10
+countSyllables("RURURURURURUrurururuRURU") // 12
+countSyllables("go") // 1
+countSyllables("dede") // 2
