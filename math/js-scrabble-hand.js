871c185844c2a879bbef1f5db7f6d105fa8a7368
Alex Devero <deveroalex@gmail.com>
2020-02-26 08:59:08 +0100

    Add scrabble-hand

---
+++ math/js-scrabble-hand.js
@@ -0,0 +1,8 @@
+function maximumScore(tileHand) {
+	return tileHand.map(hand => hand.score).reduce((acc, currVal) => acc + currVal)
+}
+
+maximumScore([{tile: 'N', score: 1}, {tile: 'K', score: 5},  {tile: 'Z', score: 10}, {tile: 'X', score: 8}, {tile: 'D', score: 2}, {tile: 'A', score: 1}, {tile: 'E', score: 1}]) // 28
+maximumScore([{tile: 'B', score: 2}, {tile: 'V', score: 4},  {tile: 'F', score: 4}, {tile: 'U', score: 1}, {tile: 'D', score: 2}, {tile: 'O', score: 1}, {tile: 'U', score: 1}]) // 15
+maximumScore([{tile: 'M', score: 3}, {tile: 'Q', score: 10},  {tile: 'O', score: 1}, {tile: 'E', score: 1}, {tile: 'E', score: 1}, {tile: 'I', score: 1}, {tile: 'A', score: 1}]) // 18
+maximumScore([{tile: 'A', score: 1}, {tile: 'E', score: 1},  {tile: 'O', score: 1}, {tile: 'E', score: 1}, {tile: 'E', score: 1}, {tile: 'I', score: 1}, {tile: 'A', score: 1}]) // 7
