d8c0226763c2c5f62e8064263ce4a127d131c67c
Alex Devero <deveroalex@gmail.com>
2020-04-07 09:08:29 +0200

    Add half-quarter-and-eighth

---
+++ numbers/js-half-quarter-and-eighth.js
@@ -0,0 +1,8 @@
+const halfQuarterEighth = (n) => [n / 2, n / 4, n / 8]
+
+halfQuarterEighth(6) // [ 3, 1.5, 0.75 ]
+halfQuarterEighth(22) // [ 11, 5.5, 2.75 ]
+halfQuarterEighth(25) // [ 12.5, 6.25, 3.125 ]
+halfQuarterEighth(18) // [ 9, 4.5, 2.25 ]
+halfQuarterEighth(98) // [ 49, 24.5, 12.25 ]
+halfQuarterEighth(14) // [ 7, 3.5, 1.75 ]
