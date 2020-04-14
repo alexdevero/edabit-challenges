9a9b66ddcbe6b400b63781b499c4fc3e6e97fd3d
Alex Devero <deveroalex@gmail.com>
2020-04-14 08:22:14 +0200

    Add atm-pin-code-validation

---
+++ regex/js-atm-pin-code-validation.js
@@ -0,0 +1,24 @@
+function validatePIN(pin) {
+	return /^(\d{4}|\d{6})$/.test(pin)
+}
+
+validatePIN('1234') // true
+validatePIN('12345') // false
+validatePIN('a234') // false
+validatePIN(') // false
+validatePIN('%234') // false
+validatePIN('`234') // false
+validatePIN('@234') // false
+validatePIN('#234') // false
+validatePIN('$234') // false
+validatePIN('*234') // false
+validatePIN('5678') // true
+validatePIN('^234') // false
+validatePIN('(234') // false
+validatePIN(')234') // false
+validatePIN('123456') // true
+validatePIN('-234') // false
+validatePIN('_234') // false
+validatePIN('+234') // false
+validatePIN('=234') // false
+validatePIN('?234') // false
