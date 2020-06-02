6ccf31a0dd068053d46481de06fd6dd195de710f
Alex Devero <deveroalex@gmail.com>
2020-06-02 08:07:23 +0200

    Add fix-error-check-whether-given-number-is-odd

---
+++ bug-fixes/js-fix-the-error--check-whether-a-given-number-is-odd.js
@@ -0,0 +1,12 @@
+function isOdd(num) {
+	return num % 2 !== 0
+}
+
+isOdd(-5) // true
+isOdd(-50) // false
+isOdd(0) // false
+isOdd(25) // true
+isOdd(-19) // true
+isOdd(12) // false
+isOdd(23) // true
+isOdd(-71) // true
