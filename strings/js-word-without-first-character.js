50f148e2c47ff5a7b5f50d7abb9d21c93dd6f6ba
Alex Devero <deveroalex@gmail.com>
2020-06-11 09:37:34 +0200

    Add word-without-first-character

---
+++ strings/js-word-without-first-character.js
@@ -0,0 +1,11 @@
+function newWord(txt) {
+	return txt.substring(1)
+}
+
+newWord("pokhara") // "okhara"
+newWord("biratnagar") // "iratnagar"
+newWord("nepal") // "epal"
+newWord("damak") // "amak"
+newWord("itahari") // "tahari"
+newWord("rasuwa") // "asuwa"
+newWord("rolpa") // "olpa"
