c255ee002665e5bcd92feb7fcaae0b84177a73b3
Alex Devero <deveroalex@gmail.com>
2020-06-14 09:08:59 +0200

    Add middle-character-of-a-string

---
+++ strings/js-middle-character-of-a-string.js
@@ -0,0 +1,10 @@
+function middleCharacter(txt){
+	return txt[txt.length / 2 - 1]
+}
+
+middleCharacter('balkot') // 'l'
+middleCharacter('himani') // 'm'
+middleCharacter('nepali') // 'p'
+middleCharacter('java') // 'a'
+middleCharacter('kane') // 'a'
+middleCharacter('stoned') // 'o'
