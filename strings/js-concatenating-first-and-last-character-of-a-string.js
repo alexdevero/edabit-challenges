4bfaedbe2348c4640464dd38e0ea499af539436e
Alex Devero <deveroalex@gmail.com>
2020-06-16 09:28:48 +0200

    Add concatenating-first-last-character-of-string

---
+++ strings/js-concatenating-first-and-last-character-of-a-string.js
@@ -0,0 +1,10 @@
+function firstLast(name) {
+	return name[0] + name[name.length - 1]
+}
+
+firstLast("ganesh") // "gh"
+firstLast("kali") // "ki"
+firstLast("shiva") // "sa"
+firstLast("vishnu") // "vu"
+firstLast("durga") // "da"
+firstLast("brahma") // "ba"
