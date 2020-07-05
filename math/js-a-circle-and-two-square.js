d756fa7ddc2e08f397966362bdf70f5d3626d3b4
Alex Devero <deveroalex@gmail.com>
2020-07-05 09:13:08 +0200

    Add a-circle-and-two-square

---
+++ math/js-a-circle-and-two-square.js
@@ -0,0 +1,8 @@
+function squareAreasDifference(r) {
+	return 2 * r ** 2
+}
+
+squareAreasDifference(5) // 50
+squareAreasDifference(6) // 72
+squareAreasDifference(7) // 98
+squareAreasDifference(17) // 578
