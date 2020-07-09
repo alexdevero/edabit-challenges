603840d6fd444e2f24c58c23ccf663407bc1c0e1
Alex Devero <deveroalex@gmail.com>
2020-07-09 09:07:14 +0200

    Add convert-a-number-to-base-2

---
+++ numbers/js-convert-a-number-to-base-2.js
@@ -0,0 +1,12 @@
+function binary(decimal) {
+	return decimal.toString(2)
+}
+
+binary(100) // "1100100"
+binary(1) // "1"
+binary(0) // "0"
+binary(69) // "1000101"
+binary(1023) // "1111111111"
+binary(511) // "111111111"
+binary(666) // "1010011010"
+binary(123) // "1111011"
