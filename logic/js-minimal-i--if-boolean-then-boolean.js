2b4b18fc4cd32cd688d5dbe2a75b58a57b783157
Alex Devero <deveroalex@gmail.com>
2020-07-19 08:08:41 +0200

    Add minimal-i--if-boolean-then-boolean

---
+++ logic/js-minimal-i--if-boolean-then-boolean.js
@@ -0,0 +1,10 @@
+const isEven = (n) => n % 2 == 0
+
+isEven(2) // true
+isEven(3) // false
+isEven(10) // true
+isEven(31) // false
+isEven(666) // true
+isEven(777) // false
+isEven(3482034) // true
+isEven(3482035) // false
