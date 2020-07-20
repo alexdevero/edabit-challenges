e4a5c47b4e25ba7bdd64a174c82d066d5de54dc5
Alex Devero <deveroalex@gmail.com>
2020-07-20 09:34:57 +0200

    Add return-something-to-me

---
+++ strings/js-return-something-to-me.js
@@ -0,0 +1,7 @@
+function giveMeSomething(a) {
+	return `something ${a}`
+}
+
+giveMeSomething("a") // "something a"
+giveMeSomething("is cooking") // "something is cooking"
+giveMeSomething(" is cooking") // "something  is cooking"
