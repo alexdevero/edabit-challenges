f1d2c0d93755e1987c0029a91416d3326f55f808
Alex Devero <deveroalex@gmail.com>
2020-08-01 09:05:45 +0200

    Add on-off-switches

---
+++ math/js-on-off-switches.js
@@ -0,0 +1,15 @@
+function posCom(num) {
+	return Math.pow(2, num)
+}
+
+posCom(5) // 32
+posCom(4) // 16
+posCom(3) // 8
+posCom(2) // 4
+posCom(1) // 2
+posCom(6) // 64
+posCom(7) // 128
+posCom(8) // 256
+posCom(9) // 512
+posCom(10) // 1024
+posCom(25) // 33554432
