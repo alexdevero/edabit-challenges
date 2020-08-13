2890daf90d2950bc28660a50927ba14b0ff9003e
Alex Devero <deveroalex@gmail.com>
2020-08-13 10:01:24 +0200

    Add number-of-stickers

---
+++ math/js-number-of-stickers.js
@@ -0,0 +1,19 @@
+function howManyStickers(n) {
+	return n * 6 * n
+}
+
+howManyStickers(1) // 6
+howManyStickers(2) // 24
+howManyStickers(3) // 54
+howManyStickers(4) // 96
+howManyStickers(5) // 150
+howManyStickers(6) // 216
+howManyStickers(7) // 294
+howManyStickers(8) // 384
+howManyStickers(9) // 486
+howManyStickers(10) // 600
+howManyStickers(11) // 726
+howManyStickers(12) // 864
+howManyStickers(13) // 1014
+howManyStickers(14) // 1176
+howManyStickers(15) // 1350
