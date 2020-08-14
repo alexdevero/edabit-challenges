54d56f4c5e6ca55e5d43d40eb0fce919d32b66a8
Alex Devero <deveroalex@gmail.com>
2020-08-14 07:55:41 +0200

    Add nth-star-number

---
+++ math/js-nth-star-number.js
@@ -0,0 +1,11 @@
+function starNumber(n) {
+	return (6 * n * (n - 1) + 1 )
+}
+
+starNumber(2) // 13
+starNumber(3) // 37
+starNumber(6) // 181
+starNumber(6) // 181
+starNumber(8) // 337
+starNumber(6) // 181
+starNumber(11) // 661
