0d60d45b95047b21113c5596bf686a9fc1aab167
Alex Devero <deveroalex@gmail.com>
2020-08-22 07:59:19 +0200

    Add examples

--- strings/js-luke-i-am-your.js
+++ strings/js-luke-i-am-your.js
@@ -8,3 +8,8 @@ function relationToLuke(name) {
 
 	return `Luke, I am your ${names[name]}.`
 }
+
+relationToLuke("Darth Vader") // "Luke, I am your father."
+relationToLuke("Leia") // "Luke, I am your sister."
+relationToLuke("Han") // "Luke, I am your brother in law."
+relationToLuke("R2D2") // "Luke, I am your droid."
