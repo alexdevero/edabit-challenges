d5de4f36dc484ae851c7e7dcc434910104694cb6
Alex Devero <deveroalex@gmail.com>
2020-09-03 09:37:02 +0200

    Add radians-to-degrees

---
+++ math/js-radians-to-degrees.js
@@ -0,0 +1,10 @@
+function radiansToDegrees(rad) {
+	return rad * 180 / Math. PI
+}
+
+radiansToDegrees(1) // 57.29577951308232
+radiansToDegrees(5) // 286.4788975654116
+radiansToDegrees(7) // 401.07045659157626
+radiansToDegrees(60) // 3437.7467707849396
+radiansToDegrees(100) // 5729.5779513082325
+radiansToDegrees(180) // 10313.240312354817
