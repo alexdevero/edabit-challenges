6f0fb13900e7e28ace436305ca30c9c513d81c1a
Alex Devero <deveroalex@gmail.com>
2020-09-13 08:15:19 +0200

    Add emotify-sentence

---
+++ strings/js-emotify-sentence.js
@@ -0,0 +1,10 @@
+function emotify(str) {
+	const emojis = {'smile': ':D', 'grin': ':)', 'sad': ':(', 'mad': ':P' }
+
+  return str.replace(/smile|grin|sad|mad/g, (word) => emojis[word])
+}
+
+emotify("Make me smile") // "Make me :D"
+emotify("Make me grin") // "Make me :)"
+emotify("Make me sad") // "Make me :("
+emotify("Make me mad") // "Make me :P"
