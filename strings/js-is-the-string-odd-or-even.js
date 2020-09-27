12f3d3207a08ec612d1b18bbf758c3a003fc4b3d
Alex Devero <deveroalex@gmail.com>
2020-09-27 09:01:19 +0200

    Add is-the-string-odd-or-even

---
+++ strings/js-is-the-string-odd-or-even.js
@@ -0,0 +1,10 @@
+function oddOrEven(s) {
+	return s.length % 2 === 0
+}
+
+oddOrEven("apples") // true
+oddOrEven("banana") // true
+oddOrEven("cherry") // true
+oddOrEven("mango") // false
+oddOrEven("peach") // false
+oddOrEven("pears") // false
