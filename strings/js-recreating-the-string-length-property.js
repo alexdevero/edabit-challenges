544fe8b903fd44cd0d4ad4cb7492034f0dd5b980
Alex Devero <deveroalex@gmail.com>
2020-09-30 08:59:57 +0200

    Add recreating-the-string-length-property

---
+++ strings/js-recreating-the-string-length-property.js
@@ -0,0 +1,11 @@
+function length(s) {
+	return s.lastIndexOf('')
+}
+
+length("Hello World") // 11
+length("Edabit") // 6
+length("wash your hands!") // 16
+length("34445") // 5
+length("   ") // 3
+length("") // 0
+length("%%$") // 3
