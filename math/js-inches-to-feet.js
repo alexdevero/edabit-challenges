9c9357b176e6885607c81b3fd34da27fb0250c58
Alex Devero <deveroalex@gmail.com>
2020-10-02 09:27:48 +0200

    Add inches-to-feet

---
+++ math/js-inches-to-feet.js
@@ -0,0 +1,10 @@
+function inchesToFeet(inches) {
+	return Math.floor(inches / 12)
+}
+
+inchesToFeet(12) // 1)
+inchesToFeet(360) // 30)
+inchesToFeet(3612) // 301
+inchesToFeet(324) // 27
+inchesToFeet(3012) // 251
+inchesToFeet(11) // 0
