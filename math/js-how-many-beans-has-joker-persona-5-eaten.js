e892b4a0902941c1f2343a815f44ed4377066bec
Alex Devero <deveroalex@gmail.com>
2020-10-12 09:02:30 +0200

    Add how-many-beans-has-joker-persona-5-eaten

---
+++ math/js-how-many-beans-has-joker-persona-5-eaten.js
@@ -0,0 +1,12 @@
+function fart(beans) {
+	return (2 * beans ** 2) + (5 * beans) + 3
+}
+
+fart(0) // 3
+fart(1) // 10
+fart(2) // 21
+fart(3) // 36
+fart(4) // 55
+fart(5) // 78
+fart(6) // 105
+fart(10) // 253
