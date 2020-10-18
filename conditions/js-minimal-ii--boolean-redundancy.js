cdc40ad13364cdbb9617c28b862cd4e3043f5b8c
Alex Devero <deveroalex@gmail.com>
2020-10-18 09:28:10 +0200

    Add minimal-ii--boolean-redundancy

---
+++ conditions/js-minimal-ii--boolean-redundancy.js
@@ -0,0 +1,12 @@
+function parity(n) {
+	return n % 2 ? 'odd' : 'even'
+}
+
+parity(2) // 'even'
+parity(3) // 'odd'
+parity(10) // 'even'
+parity(31) // 'odd'
+parity(666) // 'even'
+parity(777) // 'odd'
+parity(3482034) // 'even'
+parity(3482035) // 'odd'
