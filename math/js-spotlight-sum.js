039647642df870b13d026a68b12329fcaf52abfd
Alex Devero <deveroalex@gmail.com>
2020-11-12 09:23:06 +0100

    Add spotlight-sum

---
+++ math/js-spotlight-sum.js
@@ -0,0 +1,25 @@
+function spotlightSum(n) {
+	return n * 9
+}
+
+spotlightSum(12) // 108
+spotlightSum(13) // 117
+spotlightSum(14) // 126
+spotlightSum(15) // 135
+spotlightSum(16) // 144
+spotlightSum(17) // 153
+spotlightSum(18) // 162
+spotlightSum(19) // 171
+spotlightSum(22) // 198
+spotlightSum(23) // 207
+spotlightSum(24) // 216
+spotlightSum(25) // 225
+spotlightSum(26) // 234
+spotlightSum(27) // 243
+spotlightSum(28) // 252
+spotlightSum(29) // 261
+spotlightSum(32) // 288
+spotlightSum(33) // 297
+spotlightSum(34) // 306
+spotlightSum(35) // 315
+spotlightSum(36) // 324
