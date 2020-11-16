89c6f092b922fb00d1a4284472b5e1fc5043fd0f
Alex Devero <deveroalex@gmail.com>
2020-11-16 09:44:28 +0100

    Add number-of-squares-in-an-n-n-grid

---
+++ math/number-of-squares-in-an-n-n-grid.js
@@ -0,0 +1,11 @@
+function numberSquares(n) {
+	return (n ** 3 / 3) + (n ** 2 / 2) + (n / 6)
+}
+
+numberSquares(3) // 14
+numberSquares(10) // 385
+numberSquares(12) // 650
+numberSquares(5) // 55
+numberSquares(9) // 285
+numberSquares(11) // 506
+numberSquares(15) // 1240
