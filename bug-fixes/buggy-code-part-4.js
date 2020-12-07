7007be9f1edd64d7e3f63681c1cb257367c6115a
Alex Devero <deveroalex@gmail.com>
2020-12-07 09:09:40 +0100

    Add buggy-code-part-4

---
+++ bug-fixes/buggy-code-part-4.js
@@ -0,0 +1,9 @@
+function greeting(name) {
+  return name === "Mubashir" ? "Hello, my Love!" : "Hello, " + name + "!";
+}
+
+greeting("Matt") // "Hello, Matt!"
+greeting("Helen") // "Hello, Helen!"
+greeting("Jackie") // "Hello, Jackie!"
+greeting("Tony") // "Hello, Tony!"
+greeting("Mubashir") // "Hello, my Love!"
