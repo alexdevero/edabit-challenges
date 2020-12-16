06c90e711e0712ae8df4cbccbd6d3a6b6a40085a
Alex Devero <deveroalex@gmail.com>
2020-12-16 09:08:50 +0100

    Add basketball-points

---
+++ math/basketball-points.js
@@ -0,0 +1,9 @@
+function points(twoPointers, threePointers) {
+	return twoPointers * 2 + threePointers * 3
+}
+
+points(1, 1) // 5
+points(1, 2) // 8
+points(2, 1) // 7
+points(2, 2) // 10
+points(69, 420) // 1398
