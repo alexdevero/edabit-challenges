fd2c8d1e03b060d962c8a17dabbae511e6d5a447
Alex Devero <deveroalex@gmail.com>
2020-12-21 08:19:30 +0100

    Add return-negative

---
+++ numbers/return-negative.js
@@ -0,0 +1,12 @@
+function returnNegative(n){
+  return -Math.abs(n)
+}
+
+returnNegative(4) // -4
+returnNegative(15) // -15
+returnNegative(-4) // -4
+returnNegative(42) // -42
+returnNegative(-9) // -9
+returnNegative(0) // 0
+returnNegative(1) // -1
+returnNegative(-1) // -1
