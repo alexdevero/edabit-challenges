61de7bc9c84be946055dc26c737107bb6d437d19
Alex Devero <deveroalex@gmail.com>
2020-12-24 08:22:37 +0100

    Add father-and-son-ages

---
+++ math/father-and-son-ages.js
@@ -0,0 +1,9 @@
+function ageDifference(fAge, sAge) {
+  return Math.abs(fAge - (sAge * 2))
+}
+
+ageDifference(36,7) // 22
+ageDifference(55,30) // 5
+ageDifference(42,21) // 0
+ageDifference(22,1) // 20
+ageDifference(29,0) // 29
