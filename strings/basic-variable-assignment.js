2b39f47397ca0563e65f33ad1868ff32a6a19f2c
Alex Devero <deveroalex@gmail.com>
2021-01-10 08:20:36 +0100

    Add basic-variable-assignment

---
+++ strings/basic-variable-assignment.js
@@ -0,0 +1,11 @@
+function nameString(name) {
+	const b = "Edabit"
+	const result = name + b
+
+  return result
+}
+
+nameString("Mubashir") // "MubashirEdabit"
+nameString("Matt") // "MattEdabit"
+nameString("javaScript") // "javaScriptEdabit"
+nameString("Airforce") // "AirforceEdabit"
