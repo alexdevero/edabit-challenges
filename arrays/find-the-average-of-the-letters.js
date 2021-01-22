cf66e0d5dfc557785934f6218267fa764277886f
Alex Devero <deveroalex@gmail.com>
2021-01-22 08:18:06 +0100

    Fix syntax

--- arrays/find-the-average-of-the-letters.js
+++ arrays/find-the-average-of-the-letters.js
@@ -3,9 +3,9 @@ function averageIndex(arr) {
 		.toFixed(2)
 }
 
-Test.assertEquals(averageIndex(['a','b','c','i']), 3.75)
-Test.assertEquals(averageIndex(['e','d','a','b','i','t']), 6.83)
-Test.assertEquals(averageIndex(['o','m','g']),11.67)
-Test.assertEquals(averageIndex(['o','h','n','o']),13.0)
-Test.assertEquals(averageIndex(['t','w','e','n','t','y','t','w','e','n','t','y']),17.83)
-Test.assertEquals(averageIndex(['y','o','u','a','r','e','t','h','e','b','e','s','t']),12.62)
+averageIndex(['a','b','c','i']) // 3.75
+averageIndex(['e','d','a','b','i','t']) // 6.83
+averageIndex(['o','m','g']) // 11.67
+averageIndex(['o','h','n','o']) // 13.0
+averageIndex(['t','w','e','n','t','y','t','w','e','n','t','y']) // 17.83
+averageIndex(['y','o','u','a','r','e','t','h','e','b','e','s','t']) // 12.62
