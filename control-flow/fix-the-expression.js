e88362b05d508b333ef1908ba8a402dca9cd5292
Alex Devero <deveroalex@gmail.com>
2021-02-16 09:53:21 +0100

    Add fix-the-expression

---
+++ control-flow/fix-the-expression.js
@@ -0,0 +1,10 @@
+function isSeven(x) {
+	return x === 7 ? true : false
+}
+
+isSeven(4) // false)
+isSeven(9) // false
+isSeven(7) // true
+isSeven(10) // false
+isSeven(20) // false
+isSeven(7) // true
