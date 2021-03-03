fc4aec4e2135273e00ecbcb3f6065f24ba2d8f71
Alex Devero <deveroalex@gmail.com>
2021-03-03 09:26:56 +0100

    Add binary-addition

---
+++ math/binary-addition.js
@@ -0,0 +1,13 @@
+function addBinary(a,b) {
+  return (a + b).toString(2)
+}
+
+addBinary(1, 1) // '10'
+addBinary(1, 2) // '11'
+addBinary(4, 5) // '1001'
+addBinary(8, 20) // '11100'
+addBinary(100, 20) // '1111000'
+addBinary(40, 50) // '1011010'
+addBinary(65, 77) // '10001110'
+addBinary(40, 50) // '1011010'
+addBinary(1, 0) // '1'
