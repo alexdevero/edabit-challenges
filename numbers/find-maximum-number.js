2d63f07a1e521604937901b799b2a828c2a0e9b2
Alex Devero <deveroalex@gmail.com>
2021-03-09 09:29:15 +0100

    Add find-maximum-number

---
+++ numbers/find-maximum-number.js
@@ -0,0 +1,12 @@
+function largest(n) {
+	return +'9'.repeat(n)
+}
+
+largest(2) // 99
+largest(3) // 999
+largest(4) // 9999
+largest(5) // 99999
+largest(6) // 999999
+largest(9) // 999999999
+largest(0) // 0
+largest(null) // 0
