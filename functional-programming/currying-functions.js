485d8f62b36d4e7b854ca565008874e240b59fd8
Alex Devero <deveroalex@gmail.com>
2021-03-21 09:23:50 +0100

    Add currying-functions

---
+++ functional-programming/currying-functions.js
@@ -0,0 +1,12 @@
+// left blank for unlimited creativity!
+function multiply(arr) {
+	return function(num) {
+		return arr.map(el => el * num)
+	}
+}
+
+multiply([1, 2, 3])(1) // [1, 2, 3]
+multiply([1, 2, 3])(2) // [2, 4, 6]
+multiply([1, 2, 3])(0) // [0, 0, 0]
+multiply([4, 6, 5])(10) // [40, 60, 50]
+multiply([])(10) // [], "should return an empty array"
