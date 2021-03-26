5682d486914e22dd582100040a4056f7be23e058
Alex Devero <deveroalex@gmail.com>
2021-03-26 10:03:09 +0100

    Add sum-of-two-digit-numbers

---
+++ math/sum-of-two-digit-numbers.js
@@ -0,0 +1,23 @@
+function twoDigitSum(n) {
+	return (Math.floor(n /10)) + (n % 10)
+}
+
+twoDigitSum(45) // 9
+twoDigitSum(65) // 11
+twoDigitSum(85) // 13
+twoDigitSum(52) // 7
+
+twoDigitSum(15) // 6
+twoDigitSum(48) // 12
+twoDigitSum(33) // 6
+twoDigitSum(29) // 11
+
+twoDigitSum(81) // 9
+twoDigitSum(10) // 1
+twoDigitSum(40) // 4
+twoDigitSum(66) // 12
+
+twoDigitSum(19) // 10
+twoDigitSum(38) // 11
+twoDigitSum(67) // 13
+twoDigitSum(96) // 15
