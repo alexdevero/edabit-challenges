dc67b5166cd774df60d867651cba167286647196
Alex Devero <deveroalex@gmail.com>
2021-04-22 11:32:09 +0200

    Replace double quotes

--- arrays/lets-sort-this-array.js
+++ arrays/lets-sort-this-array.js
@@ -4,9 +4,9 @@ function ascDesNone(arr, str) {
 		arr
 }
 
-ascDesNone([4, 3, 2, 1], "Asc") // [1, 2, 3, 4])
-ascDesNone([7, 8, 11, 66], "Des") // [66, 11, 8, 7])
-ascDesNone([1, 2, 3, 4], "None") //[1, 2, 3, 4])
-ascDesNone([1, 0, 1, 0], "Asc") // [0, 0, 1, 1])
-ascDesNone([1, 2, 2, 2, 2, 2, 2], "Des") // [2, 2, 2, 2, 2, 2, 1])
-ascDesNone([9, 7, 43, 11, 16, 111, 19], "Asc") // [7, 9, 11, 16, 19, 43, 111])
+ascDesNone([4, 3, 2, 1], 'Asc') // [1, 2, 3, 4])
+ascDesNone([7, 8, 11, 66], 'Des') // [66, 11, 8, 7])
+ascDesNone([1, 2, 3, 4], 'None') //[1, 2, 3, 4])
+ascDesNone([1, 0, 1, 0], 'Asc') // [0, 0, 1, 1])
+ascDesNone([1, 2, 2, 2, 2, 2, 2], 'Des') // [2, 2, 2, 2, 2, 2, 1])
+ascDesNone([9, 7, 43, 11, 16, 111, 19], 'Asc') // [7, 9, 11, 16, 19, 43, 111])
