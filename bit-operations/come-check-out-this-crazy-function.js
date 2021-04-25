adba905aa2e23b30573ae02559b30600cf3097e6
Alex Devero <deveroalex@gmail.com>
2021-04-25 14:08:20 +0200

    Add come-check-out-this-crazy-function

---
+++ bit-operations/come-check-out-this-crazy-function.js
@@ -0,0 +1,23 @@
+function crazyfunction(a, b) {
+	return a ^ b
+}
+
+// Set 1
+crazyfunction(10, 20) // 30
+crazyfunction(17, 35) // 50
+crazyfunction(61, 233) // 212
+crazyfunction(33, 47) // 14
+crazyfunction(18, 209) // 195
+crazyfunction(91, 54) // 109
+crazyfunction(88, 123) // 35
+crazyfunction(11, 8) // 3
+
+// Set 2
+crazyfunction(56, 80) // 104
+crazyfunction(8, 45) // 37
+crazyfunction(99, 201) // 170
+crazyfunction(11, 78) // 69
+crazyfunction(49, 139) // 186
+crazyfunction(31, 50) // 45
+crazyfunction(102, 113) // 23
+crazyfunction(90, 500768) // 500858
