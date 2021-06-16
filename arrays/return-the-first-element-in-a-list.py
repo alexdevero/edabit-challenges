90b56e2cb40faa74c684711fe595cc1a02048c17
Alex Devero <deveroalex@gmail.com>
2021-06-16 17:24:04 +0200

    Add return-the-first-element-in-list

---
+++ arrays/return-the-first-element-in-a-list.py
@@ -0,0 +1,9 @@
+def get_first_value(number_list):
+	return number_list[0]
+
+get_first_value([1, 2, 3]) # 1
+get_first_value([80, 5, 100]) # 80
+get_first_value([-500, 0, 50]) # -500
+get_first_value([5, 2, 3]) # 5
+get_first_value([75675, 5, 100]) # 75675
+get_first_value([-52320, 0, 50]) # -52320
