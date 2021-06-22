451a76b27ca73dd20048ff1168eb56705497ab2c
Alex Devero <deveroalex@gmail.com>
2021-06-22 17:22:35 +0200

    Add string-to-integer-and-vice-versa

---
+++ strings/string-to-integer-and-vice-versa.py
@@ -0,0 +1,14 @@
+def to_int(txt):
+	return int(txt)
+
+def to_str(num):
+	return str(num)
+
+to_int("37") # 37
+to_int("113") # 113
+to_int("5") # 5
+to_int("5231") # 5231
+to_str(37) # "37"
+to_str(113) # "113"
+to_str(5) # "5"
+to_str(5231) # "5231"
