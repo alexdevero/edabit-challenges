d45eac89a06de809124d28279ea02303d3c05cb7
Alex Devero <deveroalex@gmail.com>
2021-06-23 13:54:28 +0200

    Add basketball-points

---
+++ math/basketball-points.py
@@ -0,0 +1,8 @@
+def points(twopointers, threepointers):
+	return twopointers * 2 + threepointers * 3
+
+points(1, 1 #,5
+points(1, 2) # 8
+points(2, 1) # 7
+points(2, 2) # 10
+points(69, 420) # 1398
