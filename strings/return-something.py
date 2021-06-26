c5374bbe111271cbc6bfebd317ac82be726a8d83
Alex Devero <deveroalex@gmail.com>
2021-06-26 17:12:39 +0200

    Add return-something

---
+++ strings/return-something.py
@@ -0,0 +1,6 @@
+def give_me_something(a):
+	return "something " + a
+
+give_me_something("a") # "something a"
+give_me_something("is cooking") # "something is cooking"
+give_me_something(" is cooking") # "something  is cooking"
