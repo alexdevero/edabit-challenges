45b498d0c20c80102800ec44265f8563e214c8b4
Alex Devero <deveroalex@gmail.com>
2021-06-27 17:15:18 +0200

    Add find-smallest-number-in-list

---
+++ numbers/find-the-smallest-number-in-a-list.py
@@ -0,0 +1,13 @@
+def find_smallest_num(lst):
+	return min(lst)
+
+find_smallest_num([34, 15, 88, 2]) # 2
+find_smallest_num([34, -345, -1, 100]) # -345
+find_smallest_num([-76, 1.345, 1, 0]) # -76
+find_smallest_num([0.4356, 0.8795, 0.5435, -0.9999]) # -0.9999
+find_smallest_num([7, 7, 7]) # 7
+find_smallest_num([4, 6, 1, 3, 4, 5, 5, 1]) # 1
+find_smallest_num([-4, -6, -8, -1]) # -8
+find_smallest_num([54, 76, 23, 54]) # 23
+find_smallest_num([100]) # 100
+find_smallest_num([0, 1, 2, 3, 4, 5]) # 0
