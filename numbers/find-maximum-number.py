a49ec1a47b3a184d09bd85f80f1ac2d7a971dfbc
Alex Devero <deveroalex@gmail.com>
2021-06-28 17:30:10 +0200

    Add find-maximum-number

---
+++ numbers/find-maximum-number.py
@@ -0,0 +1,10 @@
+def findLargestNum(nums):
+	return max(nums)
+
+findLargestNum([4, 5, 1, 3]) # 5
+findLargestNum([13, 27, 18, 26]) # 27
+findLargestNum([32, 35, 37, 39]) # 39
+findLargestNum([1000, 1001, 857, 1]) # 1001
+findLargestNum([27364, 837363, 736736, 73635]) # 837363
+findLargestNum([30, 2, 40, 3]) # 40
+findLargestNum([0, 1, 0, 0, 1]) # 1
