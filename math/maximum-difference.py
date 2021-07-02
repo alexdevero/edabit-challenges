0db8a0562082c565f9086d100f2d1e2eb8b4d04c
Alex Devero <deveroalex@gmail.com>
2021-07-02 15:20:03 +0200

    Add maximum-difference

---
+++ math/maximum-difference.py
@@ -0,0 +1,15 @@
+def difference(nums):
+	return max(nums) - min(nums)
+
+difference([-9, -8, 6, -9, 15, 6]) # 24
+difference([-5, 6, 18, 4, 16, -2]) # 23
+difference([-2, 20, -9, -9, -2, -7]) # 29
+difference([4, -2, 11, -9, 15, 2]) # 24
+difference([15, 10, 3, -6, 6, 19]) # 25
+difference([1, 7, 18, -1, -2, 9]) # 20
+difference([5, 1, -9, 7, -8, -10]) # 17
+difference([-4, 17, -4, 20, -7, 0]) # 27
+difference([-2, 11, 11, -3, -3, -3]) # 14
+difference([1, 15, 14, 20, 10, 6]) # 19
+difference([4, 17, 12, 2, 10, 2]) # 15
+difference([-3, 3, 20, 10, 0, 17]) # 23
