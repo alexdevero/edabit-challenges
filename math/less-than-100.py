332467835e84c9a36fd8911ba5892ce8e56f7d48
Alex Devero <deveroalex@gmail.com>
2021-07-03 17:22:29 +0200

    Add less-than-100

---
+++ math/less-than-100.py
@@ -0,0 +1,15 @@
+def less_than_100(a, b):
+	return a + b < 100
+
+less_than_100(5, 57) # true
+less_than_100(77, 30) # false
+less_than_100(0, 59) # true
+less_than_100(78, 35) # false
+less_than_100(63, 11) # true
+less_than_100(37, 99) # false
+less_than_100(52, 11) # true
+less_than_100(82, 95) # false
+less_than_100(17, 44) # true
+less_than_100(74, 53) # false
+less_than_100(3, 77) # true
+less_than_100(25, 80) # false
