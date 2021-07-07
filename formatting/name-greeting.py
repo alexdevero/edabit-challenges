df5ba8211e9852ff72b17d13a97c5aca7a49c7ea
Alex Devero <deveroalex@gmail.com>
2021-07-07 16:56:33 +0200

    Add name-greeting.py

---
+++ formatting/name-greeting.py
@@ -0,0 +1,7 @@
+def hello_name(name):
+	return "Hello " + name + "!"
+
+hello_name("Gerald") # "Hello Gerald!"
+hello_name("Fatima") # "Hello Fatima!"
+hello_name("Ed") # "Hello Ed!"
+hello_name("Tiffany") # "Hello Tiffany!"
