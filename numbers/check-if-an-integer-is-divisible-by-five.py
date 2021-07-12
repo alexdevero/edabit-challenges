16c2d8465de3cdeed7b41a1b6ec5a5a055fae09e
Alex Devero <deveroalex@gmail.com>
2021-07-12 14:58:23 +0200

    Add check-if-int-divisible-by-five

---
+++ numbers/check-if-an-integer-is-divisible-by-five.py
@@ -0,0 +1,10 @@
+def divisible_by_five(n):
+	return n % 5 == 0
+
+divisible_by_five(7) # False
+divisible_by_five(5) # True
+divisible_by_five(15) # True
+divisible_by_five(33) # False
+divisible_by_five(-18) # False
+divisible_by_five(999) # False
+divisible_by_five(2) # False
