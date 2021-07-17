f86631a37b0a4d8a20408a69c52b591098ad4f7e
Alex Devero <deveroalex@gmail.com>
2021-07-17 13:44:00 +0200

    Add divides-evenly

---
+++ math/divides-evenly.py
@@ -0,0 +1,23 @@
+def divides_evenly(a, b):
+	return a % b == 0
+
+divides_evenly(98, 7) # True
+divides_evenly(87, 49) # False
+divides_evenly(34, 14) # False
+divides_evenly(78, 6) # True
+divides_evenly(30, 4) # False
+divides_evenly(87, 73) # False
+divides_evenly(74, 7) # False
+divides_evenly(87, 29) # True
+divides_evenly(48, 24) # True
+divides_evenly(99, 20) # False
+divides_evenly(98, 49) # True
+divides_evenly(100, 6) # False
+divides_evenly(64, 4) # True
+divides_evenly(70, 35) # True
+divides_evenly(38, 38) # True
+divides_evenly(29, 3) # False
+divides_evenly(20, 8) # False
+divides_evenly(66, 50) # False
+divides_evenly(95, 1) # True
+divides_evenly(58, 2) # True
