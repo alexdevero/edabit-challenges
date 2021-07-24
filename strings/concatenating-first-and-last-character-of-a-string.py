d5e91e26d27b724eb4068a4be98a9f61ae358543
Alex Devero <deveroalex@gmail.com>
2021-07-24 17:07:10 +0200

    Add concatenating-first-and-last-char

---
+++ strings/concatenating-first-and-last-character-of-a-string.py
@@ -0,0 +1,9 @@
+def first_last(name):
+  return name[0] + name[-1]
+
+first_last("ganesh") # "gh"
+first_last("kali") # "ki"
+first_last("shiva") # "sa"
+first_last("vishnu") # "vu"
+first_last("durga") # "da"
+first_last("brahma") # "ba"
