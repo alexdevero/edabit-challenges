2b7209230ead3c32a1dd22668f5e0b38fdccc8a9
Alex Devero <deveroalex@gmail.com>
2021-07-28 16:14:10 +0200

    Add minimal-i--if-boolean-then-boolean

---
+++ conditions/minimal-i--if-boolean-then-boolean.py
@@ -0,0 +1,11 @@
+def is_even(n):
+	return n % 2 == 0
+
+is_even(2) # True
+is_even(3) # False
+is_even(10) # True
+is_even(31) # False
+is_even(666) # True
+is_even(777) # False
+is_even(3482034) # True
+is_even(3482035) # False
