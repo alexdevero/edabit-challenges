0413c7575d65711a715a28bde6004015ff36b529
Alex Devero <deveroalex@gmail.com>
2021-07-29 17:15:53 +0200

    Add sort-numbers-in-ascending-order

---
+++ arrays/sort-numbers-in-ascending-order.py
@@ -0,0 +1,12 @@
+def sort_nums_ascending(lst):
+	return sorted(lst)
+
+sort_nums_ascending([1, 2, 10, 50, 5]) # [1, 2, 5, 10, 50])
+sort_nums_ascending([80, 29, 4, -95, -24, 85]) # [-95, -24, 4, 29, 80, 85])
+sort_nums_ascending([]) # [])
+sort_nums_ascending([47, 51, -17, -16, 91, 47, -85, -8, -16, -27]) # [-85, -27, -17, -16, -16, -8, 47, 47, 51, 91])
+sort_nums_ascending([-51, -73, 65, 69, -76, 74, -14]) # [-76, -73, -51, -14, 65, 69, 74])
+sort_nums_ascending([45, 98, 35, 65, 97, 21, 33]) # [21, 33, 35, 45, 65, 97, 98])
+sort_nums_ascending([-23, -69, -54, -2, -32]) # [-69, -54, -32, -23, -2])
+sort_nums_ascending([-21, -9, -96]) # [-96, -21, -9])
+sort_nums_ascending([0]) # [0])
