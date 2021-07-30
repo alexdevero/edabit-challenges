1779fc4c574cf181aec25a475c9d84c93724ca2b
Alex Devero <deveroalex@gmail.com>
2021-07-30 16:27:38 +0200

    Add buggy-code-part-4

---
+++ bug-fixes/buggy-code-part-4.py
@@ -0,0 +1,8 @@
+def greeting(name):
+	if name == "Mubashir":
+		return "Hello, my Love!"
+	return "Hello, " + name + "!"
+
+greeting("Matt") # "Hello, Matt!"
+greeting("Helen") # "Hello, Helen!"
+greeting("Mubashir") # "Hello, my Love!"
