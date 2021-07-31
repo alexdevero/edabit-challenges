d169d0df75d05e5065167c2b20757b33e2826008
Alex Devero <deveroalex@gmail.com>
2021-07-31 17:20:31 +0200

    Add basic-variable-assignment

---
+++ language-fundamentals/basic-variable-assignment.py
@@ -0,0 +1,9 @@
+def name_string(name):
+	  b = "Edabit"
+	  result = name + b
+	  return result
+
+name_string("Eliott") # "EliottEdabit"
+name_string("Matt") # "MattEdabit"
+name_string("python") # "pythonEdabit"
+name_string("Airforce") # "AirforceEdabit"
