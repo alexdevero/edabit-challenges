41d4e49eee66b0a8b9f3174e6a32359c53163d51
Alex Devero <deveroalex@gmail.com>
2021-08-06 17:12:32 +0200

    Add inches-to-feet

---
+++ math/inches-to-feet.py
@@ -0,0 +1,9 @@
+def inches_to_feet(inches):
+	return inches //12
+
+inches_to_feet(12) # 1
+inches_to_feet(360) # 30
+inches_to_feet(3612) # 301
+inches_to_feet(324) # 27
+inches_to_feet(3012) # 251
+inches_to_feet(11) # 0
