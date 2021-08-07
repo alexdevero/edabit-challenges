89a6fa9b8b00583503375d5e39d1048e9d372857
Alex Devero <deveroalex@gmail.com>
2021-08-07 17:16:06 +0200

    Add fix-the-error--check-whether

---
+++ bug-fixes/fix-the-error--check-whether-a-given-number-is-odd.py
@@ -0,0 +1,11 @@
+def is_odd(num):
+  return num % 2 != 0
+
+is_odd(-5) # True
+is_odd(-50) # False
+is_odd(0) # False
+is_odd(25) # True
+is_odd(-19) # True
+is_odd(12) # False
+is_odd(23) # True
+is_odd(-71) # True
