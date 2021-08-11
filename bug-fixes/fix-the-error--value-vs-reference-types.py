020dab393991ac74ab1dcef5b844d8c4bea88aaf
Alex Devero <deveroalex@gmail.com>
2021-08-11 17:15:58 +0200

    Add fix-the-error--value-vs-reference

---
+++ bug-fixes/fix-the-error--value-vs-reference-types.py
@@ -0,0 +1,8 @@
+# Fix this broken code!
+def check_equals(lst1, lst2):
+	return lst1 == lst2
+
+check_equals([1, 2], [1, 3]) # False
+check_equals([1, 2], [1, 2]) # True
+check_equals([4, 5, 6], [4, 5, 6]) # True
+check_equals([4, 7, 6], [4, 5, 6]) # False
