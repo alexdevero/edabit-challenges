0a87125969926fbfbfe4e7ead957fdee7ee26dca
Alex Devero <deveroalex@gmail.com>
2021-08-12 17:15:33 +0200

    Add is-the-last-character-an-n

---
+++ strings/is-the-last-character-an-n.py
@@ -0,0 +1,10 @@
+def is_last_character_n(word):
+	return word.endswith('n')
+
+is_last_character_n("Aiden") # True
+is_last_character_n("Roxy") # False
+is_last_character_n("Bert") # False
+is_last_character_n("Dean") # True
+is_last_character_n("Ian") # True
+is_last_character_n("Brian") # True
+is_last_character_n("Daniel") # False
