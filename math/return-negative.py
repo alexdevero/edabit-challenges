7dd99b7aa5b09762531ba94ecdb325baa20fa49c
Alex Devero <deveroalex@gmail.com>
2021-08-15 16:28:23 +0200

    Add return-negative

---
+++ math/return-negative.py
@@ -0,0 +1,11 @@
+def return_negative(n):
+	return abs(n) * -1
+
+return_negative(4) # -4
+return_negative(15) # -15
+return_negative(-4) # -4
+return_negative(42) # -42
+return_negative(-9) # -9
+return_negative(0) # 0
+return_negative(1) # -1
+return_negative(-1) # -1
