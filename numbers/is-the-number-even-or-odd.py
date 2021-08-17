ba4977c38323d6dcf0b48a0b976f6062f0792926
Alex Devero <deveroalex@gmail.com>
2021-08-17 17:22:57 +0200

    Add is-the-number-even-or-odd

---
+++ numbers/is-the-number-even-or-odd.py
@@ -0,0 +1,20 @@
+def isEvenOrOdd(num):
+	if num % 2 == 0:
+		return 'even'
+	else:
+		return 'odd'
+
+isEvenOrOdd(3) # "odd"
+isEvenOrOdd(0) # "even"
+isEvenOrOdd(7) # "odd"
+isEvenOrOdd(12) # "even"
+isEvenOrOdd(6474) # "even"
+isEvenOrOdd(3) # "odd"
+isEvenOrOdd(301) # "odd"
+isEvenOrOdd(-3) # "odd"
+isEvenOrOdd(-0) # "even"
+isEvenOrOdd(-7) # "odd"
+isEvenOrOdd(-12) # "even"
+isEvenOrOdd(-6474) # "even"
+isEvenOrOdd(-3) # "odd"
+isEvenOrOdd(-301) # "odd"
