0fc931656bdd3031fe76d3b0549151f027763f33
Alex Devero <deveroalex@gmail.com>
2021-08-18 17:27:24 +0200

    Add burrrrrrrp

---
+++ strings/burrrrrrrp.py
@@ -0,0 +1,10 @@
+def long_burp(num):
+	return "Bu" + "r" * num + "p"
+
+long_burp(3) # "Burrrp"
+long_burp(5) # "Burrrrrp"
+long_burp(9) # "Burrrrrrrrrp"
+long_burp(10) # "Burrrrrrrrrrp"
+long_burp(13) # "Burrrrrrrrrrrrrp"
+long_burp(18) # "Burrrrrrrrrrrrrrrrrrp"
+long_burp(1) # "Burp"
