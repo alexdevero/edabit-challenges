2b689c26e14af0ff745c872f8eeb40585fac2494
Alex Devero <deveroalex@gmail.com>
2021-08-19 17:19:16 +0200

    Add word-without-first-character

---
+++ strings/word-without-first-character.py
@@ -0,0 +1,10 @@
+def new_word(word):
+	return word[1:]
+
+new_word("pokhara") # "okhara"
+new_word("biratnagar") # "iratnagar"
+new_word("nepal") # "epal"
+new_word("damak") # "amak"
+new_word("itahari") # "tahari"
+new_word("rasuwa") # "asuwa"
+new_word("rolpa") # "olpa"
