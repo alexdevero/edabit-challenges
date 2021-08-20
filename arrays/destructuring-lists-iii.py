07645764829bbb33523e1f680fc265cd27a37dd3
Alex Devero <deveroalex@gmail.com>
2021-08-20 17:21:35 +0200

    Add destructuring-lists-iii

---
+++ arrays/destructuring-lists-iii.py
@@ -0,0 +1,9 @@
+# DO NOT change arr
+# DO NOT USE lips = arr[2]
+# "eyes", "nose", and "ears" should not be assigned to anything
+
+arr = ["eyes", "nose", "lips", "ears"]
+'''Create variable lips here and assign it to arr[2]'''
+_, _, lips, _= arr
+
+lips # arr[2]
