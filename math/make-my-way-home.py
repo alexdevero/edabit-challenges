fea8ff5d7765f86fa78fd9937c762f8b2d2b8909
Alex Devero <deveroalex@gmail.com>
2021-08-31 17:09:49 +0200

    Add make-my-way-home

---
+++ math/make-my-way-home.py
@@ -0,0 +1,23 @@
+def distance_home(lst):
+	return abs(sum(lst))
+
+distance_home([2, 4, 2, 5]) # 13
+distance_home([-1, -4, -3, -2]) # 10
+distance_home([3, 4, -5, -2]) # 0
+distance_home([12, 12]) # 24
+distance_home([10, 8, -4, -10, -15, 14, -13]) # 10
+distance_home([-3, -6, -15, -14, -11, 3, 0, -15, 0]) # 61
+distance_home([-11, -4, -14, 4, -2, 7, 0, -1, 10, -1]) # 12
+distance_home([-7, 1, 0, -9, 0, 0]) # 15
+distance_home([-15, -7, 4, -3, -8]) # 29
+distance_home([-11, 10, -6, 8, 0, 12, -11]) # 2
+distance_home([-4, -5, -14, -13]) # 36
+distance_home([5, 8]) # 13
+distance_home([10, -15, -3, 2, 4, -15, 11, 1]) # 5
+distance_home([9, 13, -11, -11, -10, -12, 15, -3]) # 10
+distance_home([13, -1, -12, -4, 0, 10, 8, 10]) # 24
+distance_home([-9, 1, -10, 10, -9, -10, 0, 11, -2, -3]) # 21
+distance_home([13, 2, 12, 12, -15, 11, 9, 0, 13]) # 57
+distance_home([0, 5, 8, -3]) # 10
+distance_home([-10, 2, -1, -12]) # 21
+distance_home([-9, -11, 11, -15, -8]) # 32
