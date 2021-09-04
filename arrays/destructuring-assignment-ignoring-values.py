febd9268d06a5a4df7bdd0ac41d15e1660520bc4
Alex Devero <deveroalex@gmail.com>
2021-09-04 17:10:40 +0200

    Add destructuring-assignment-ign

---
+++ arrays/destructuring-assignment-ignoring-values.py
@@ -0,0 +1,7 @@
+# DO NOT USE first = writeyourcodehere[0]
+# DO NOT USE last = writeyourcodehere[-1]
+# Check Resources Tab for HELP
+first, *_, last = [1, 2, 3, 4, 5, 6, 7, 8]
+
+first # 1
+last # 8
