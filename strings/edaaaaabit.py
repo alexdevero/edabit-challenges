e2204c4def0de987214d0741f9fceea8ea490793
Alex Devero <deveroalex@gmail.com>
2021-09-07 11:04:17 +0200

    Add edaaaaabit

---
+++ strings/edaaaaabit.py
@@ -0,0 +1,8 @@
+def how_many_times(num):
+	return "Ed" + "a" * num + "bit"
+
+how_many_times(5) # "Edaaaaabit"
+how_many_times(15) # "Edaaaaaaaaaaaaaaabit"
+how_many_times(10) # "Edaaaaaaaaaabit"
+how_many_times(23) # "Edaaaaaaaaaaaaaaaaaaaaaaabit"
+how_many_times(1) # "Edabit"
