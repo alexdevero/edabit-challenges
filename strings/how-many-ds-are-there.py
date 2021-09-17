923d2ab64a484904e27ebe503806cc65e3be1e17
Alex Devero <deveroalex@gmail.com>
2021-09-17 17:28:03 +0200

    Add how-many-ds-are-there

---
+++ strings/how-many-ds-are-there.py
@@ -0,0 +1,6 @@
+def count_d(sentence):
+	return sentence.lower().count('d')
+
+count_d("My friend Dylan got distracted at school") # 4
+count_d("Debris was scattered all over the place.") # 2
+count_d("The rodents hibernated in their den.") # 3
