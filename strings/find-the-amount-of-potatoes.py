584e97bfc9de278445aaeac9311bbefff49c87b1
Alex Devero <deveroalex@gmail.com>
2021-09-20 17:41:45 +0200

    Add find-the-amount-of-potatoes

---
+++ strings/find-the-amount-of-potatoes.py
@@ -0,0 +1,9 @@
+def potatoes(potato):
+	return potato.count("potato")
+
+potatoes("potato") # 1
+potatoes("potatopotatocherry") # 2 
+potatoes("potatopotatopotatoorange") # 3
+potatoes("potatopotatobananapotatopotato") # 4
+potatoes("potatopotatomangopotatopotatopotato") # 5
+potatoes("potatocucumberpotatopotatopotatopotatopotato") # 6
