e1458128bba06e47602e6927bfaa03ea800bcd64
Alex Devero <deveroalex@gmail.com>
2021-09-23 17:41:41 +0200

    Add smash-factor

---
+++ math/smash-factor.py
@@ -0,0 +1,26 @@
+def smash_factor(bs, cs):
+	return round(bs / cs, 2)
+
+smash_factor(139.4, 93.8) # 1.49
+smash_factor(181.2, 124.5) # 1.46
+smash_factor(154.7, 104.3) # 1.48
+smash_factor(218.1, 278.3) # 0.78
+smash_factor(171.4, 171.6) # 1.0
+smash_factor(218.0, 162.2) # 1.34
+smash_factor(263.2, 272.7) # 0.97
+smash_factor(199.4, 166.0) # 1.2
+smash_factor(279.3, 103.9) # 2.69
+smash_factor(192.8, 162.6) # 1.19
+smash_factor(155.4, 272.2) # 0.57
+smash_factor(285.8, 232.5) # 1.23
+smash_factor(128.0, 136.1) # 0.94
+smash_factor(153.8, 129.7) # 1.19
+smash_factor(209.7, 164.3) # 1.28
+smash_factor(287.4, 193.6) # 1.48
+smash_factor(178.1, 202.2) # 0.88
+smash_factor(111.8, 101.5) # 1.1
+smash_factor(130.7, 284.2) # 0.46
+smash_factor(139.3, 148.0) # 0.94
+smash_factor(183.7, 198.0) # 0.93
+smash_factor(106.5, 250.2) # 0.43
+smash_factor(170.7, 274.8) # 0.62
